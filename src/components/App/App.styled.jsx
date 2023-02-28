80bd0c44d2b97527ef9a6bba8ca664cbb3d431fb
OsokinNikita <110087045+OsokinNikita@users.noreply.github.com>
2023-02-28 12:45:03 +0200

    up changes

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -3,12 +3,13 @@ import styled from '@emotion/styled';
 export const Wrapper = styled.div`
   max-width: 800px;
   background-color: ${p => p.theme.colors.backgroundColorsPrimary};
-  padding: 10px 20px 20px;
+  padding: 80px;
   margin: 0 auto;
-  border-radius: 10px;
+  border-radius: 60px;
   box-shadow: ${p => p.theme.insideShadow};
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  color: rgba(204, 0, 255, 0.675);
 `;
--- src/contans/Thema.js
+++ src/contans/Thema.js
@@ -1,13 +1,13 @@
 export const theme = {
   colors: {
-    btnСolorText: '#0b0c0', // колір кнопок #0b0c0
-    backgroundColorsPrimary: '#0b0c10', // колір фону #0b0c10
-    backgroundColors: '#1f2833', // колір фону карток
-    colorText: '#66fcc3', // колір тексту #45a29e
-    colorElement: '#66fcc3', // колір інпутів #66fcf1
-    white: '#fff', // колір тексту списку
+    btnСolorText: '#000000', // колір кнопок #0b0c0
+    backgroundColorsPrimary: '#ffffff', // колір фону #0b0c10
+    backgroundColors: '#ffffff', // колір фону карток
+    colorText: '#000000', // колір тексту #45a29e
+    colorElement: 'rgba(204, 0, 255, 0.675)', // колір інпутів #66fcf1
+    white: 'rgba(204, 0, 255, 0.675)', // колір тексту списку
   },
-  boxShadow: '1px 1px 4px 1px rgba(69,  162,  158, 0.93)',
-  sectionShadow: '0px 0px 19px 0px #66fcc3', // основний колір тіні gba(69, 162, 158, 1)
-  insideShadow: 'inset 0px 0px 19px 0px #66fcc3', // основний колір тіні gba(69, 162, 158, 1)
+  boxShadow: '1px 1px 4px 1px rgba(204, 0, 255, 0.675)',
+  sectionShadow: '0px 0px 5px 0px rgba(204, 0, 255, 0.675)', // основний колір тіні gba(69, 162, 158, 1)
+  insideShadow: 'inset 0px 0px 6px 0px rgba(204, 0, 255, 0.675)', // основний колір тіні gba(69, 162, 158, 1)
 };
